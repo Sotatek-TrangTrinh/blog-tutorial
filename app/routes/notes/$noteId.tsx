6dcaa9226d76db185019a03c7b9d8fe653313e22
Sotatek-TrangTrinh <trang.trinh@sotatek.com>
2022-11-04 02:06:28 +0700

    remove tiny-invariant

--- app/routes/notes/$noteId.tsx
+++ app/routes/notes/$noteId.tsx
@@ -1,14 +1,12 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { Form, useCatch, useLoaderData } from "@remix-run/react";
-import invariant from "tiny-invariant";
 
 import { deleteNote, getNote } from "~/models/note.server";
 import { requireUserId } from "~/session.server";
 
 export async function loader({ request, params }: LoaderArgs) {
   const userId = await requireUserId(request);
-  invariant(params.noteId, "noteId not found");
 
   const note = await getNote({ userId, id: params.noteId });
   if (!note) {
@@ -19,7 +17,6 @@ export async function loader({ request, params }: LoaderArgs) {
 
 export async function action({ request, params }: ActionArgs) {
   const userId = await requireUserId(request);
-  invariant(params.noteId, "noteId not found");
 
   await deleteNote({ userId, id: params.noteId });
 
